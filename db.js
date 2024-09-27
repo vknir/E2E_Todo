27ffa756afd5d6c9d093142f9f049c2f34c99fc3
Utkarsh Kumar <utkarsh.kumar0110@gmail.com>
2024-09-27 11:28:37 +0530

    2nd session, updated Schema

--- db.js
+++ db.js
@@ -1,2 +1,23 @@
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose') 
 
+const UserSchema = new mongoose.Schema({
+    username: {
+        type:String,
+        unique:true
+    },
+    password: {
+        type:String,
+        required:true
+    },
+    todos:Object
+})
+
+const TodoListSchema = new mongoose.Schema({
+    userID: Object,
+    todos:Object
+}) 
+
+const userModel = mongoose.model('user',UserSchema)
+const todolistModel = mongoose.model('todolist',TodoListSchema)
+
+module.exports={userModel, todolistModel}
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,49 @@
 const {Router} = require('express');
 const userRouter= Router();
-
 const  {z}= require('zod')
-const bcrypte= require('bcrypt');
+const bcrypt= require('bcrypt');
+const bodyParser = require('body-parser')
 
 
 const {JWT_SECRET}=require('../config') 
+const {userModel} =require('../db')
 
+userRouter.use(bodyParser.urlencoded({extended : false}))
 
-userRouter.post('/signup', (req, res)=>{
+userRouter.post('/signup', async (req, res)=>{
 
     const {username, password} = req.body
 
+    const  userCredValidator= z.object({
+        username :z.string(),
+        password : z.string()
+    })
+
+    try{
+        userCredValidator.parse({username, password})
+
+        bcrypt.hash(password,3, async (err, result)=>{
+            if(err)
+            {
+                res.json({message:"Error while hashing password"})
+            }
+            if( result)
+            {
+                await userModel.create({
+                    username: username, 
+                    password:result
+                });
+                res.json({message:"User added successfully!"})
+            }
+        })
+
+    }catch(e)
+    {
+        console.log(e)
+        res.json({
+            message: "Invalid credentails"
+        })
+    }
 })
 
 
